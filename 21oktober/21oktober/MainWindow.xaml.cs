e4d9d2ddd74c957020779af99ab2c91d389e24fe
roosjamro <roos.jamro@student.ucll.be>
2024-10-21 14:02:55 +0200

    DEMO

---
+++ 21oktober/21oktober/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _21oktober
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameTextBox.Text.Equals("admin") && PasswordTextBox.Text.Equals("admin"))
+            {
+                InfoTextBlock.Text = "U wordt ingelogd.";
+            }
+        }
+
+        private void LogOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTextBlock.Text = "";
+        }
+    }
+}
+
